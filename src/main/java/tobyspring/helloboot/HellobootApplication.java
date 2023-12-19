f5f50486a4751b1afa2d41865192a677cebb158b
bloodfinger8 <didwodn8822@gmail.com>
2023-12-19 22:54:55 +0900

    ADD - 스프링 컨테이너 생성 및 빈 추가

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,28 +4,35 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.ApplicationContextFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.http.MediaType;
 
 import java.io.IOException;
 
 public class HellobootApplication {
 
 	public static void main(String[] args) {
+		GenericApplicationContext applicationContext = new GenericApplicationContext();
+		applicationContext.registerBean(HelloController.class);
+		applicationContext.refresh(); //구성 정보를 이용해 컨테이너 초기화 작업 수행
+
 		ServletWebServerFactory factory = new TomcatServletWebServerFactory();
 		WebServer webServer = factory.getWebServer(servletContext -> {
-			HelloController helloController = new HelloController();
 			servletContext.addServlet("hello", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					//인증, 보안, 다국어, 공통 등의 작업
 					if (req.getRequestURI().equals("/hello") && req.getMethod().equals("GET")) {
 						String name = req.getParameter("name");
-						helloController.hello(name);
 
-						resp.setStatus(HttpServletResponse.SC_OK);
-						resp.setHeader("Content-type", "text/plain");
-						resp.getWriter().println("Hello " + name);
+						HelloController HelloControllerBean = applicationContext.getBean(HelloController.class);
+
+						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(HelloControllerBean.hello(name));
 					} else if (req.getRequestURI().equals("/user")) {
 						// ...
 					} else {
