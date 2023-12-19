0a4d2c59926425a7552ed9a5fe38fe4bcfb457e4
bloodfinger8 <didwodn8822@gmail.com>
2023-12-19 20:08:48 +0900

    INIT - 기본 서블릿 컨텍스트  등록 및 실행

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,8 +1,35 @@
 package tobyspring.helloboot;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import java.io.IOException;
+
 public class HellobootApplication {
 
 	public static void main(String[] args) {
-		System.out.println("containerless web application start");
+		ServletWebServerFactory factory = new TomcatServletWebServerFactory();
+		WebServer webServer = factory.getWebServer(servletContext ->
+				servletContext.addServlet("hello", new HttpServlet() {
+					@Override
+					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+						String name = req.getParameter("name");
+
+						resp.setStatus(HttpServletResponse.SC_OK);
+						resp.setHeader("Content-type", "text/plain");
+						resp.getWriter().println("Hello " + name);
+					}
+				}).addMapping("/hello")
+		);
+		webServer.start();
 	}
 }
