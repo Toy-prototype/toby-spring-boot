5a3ff27e72b46ab992e9cbe67a12d0f43f3c649e
bloodfinger8 <didwodn8822@gmail.com>
2023-12-19 20:35:26 +0900

    INIT - 프런트 컨트롤러 전환

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,15 +1,11 @@
 package tobyspring.helloboot;
 
-import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 import java.io.IOException;
@@ -18,17 +14,26 @@ public class HellobootApplication {
 
 	public static void main(String[] args) {
 		ServletWebServerFactory factory = new TomcatServletWebServerFactory();
-		WebServer webServer = factory.getWebServer(servletContext ->
-				servletContext.addServlet("hello", new HttpServlet() {
-					@Override
-					protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		WebServer webServer = factory.getWebServer(servletContext -> {
+			HelloController helloController = new HelloController();
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals("GET")) {
 						String name = req.getParameter("name");
+						helloController.hello(name);
 
 						resp.setStatus(HttpServletResponse.SC_OK);
 						resp.setHeader("Content-type", "text/plain");
 						resp.getWriter().println("Hello " + name);
+					} else if (req.getRequestURI().equals("/user")) {
+						// ...
+					} else {
+						resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
 					}
-				}).addMapping("/hello")
+				}
+			}).addMapping("/*");
+			}
 		);
 		webServer.start();
 	}
