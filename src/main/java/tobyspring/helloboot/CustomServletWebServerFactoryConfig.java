a072a13b75747066013a9ec63e0795803ec1a9cf
bloodfinger8 <didwodn8822@gmail.com>
2023-12-22 20:30:44 +0900

    ADD - @ConditionalOnMissingBean 을 사용해 자동구성 Bean 대체

---
+++ src/main/java/tobyspring/helloboot/CustomServletWebServerFactoryConfig.java
@@ -0,0 +1,16 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomServletWebServerFactoryConfig {
+    @Bean
+    public ServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setPort(9090);
+        return factory;
+    }
+}
